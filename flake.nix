299ff24f5b5959fc1b220ba89fe4b3e286edf7e6
Strahinja Popovic <227760+letac@users.noreply.github.com>
2026-02-18 04:34:12 +0100

    ocrodjvu has become nixpkgs pkgs

--- flake.nix
+++ flake.nix
@@ -9,80 +9,77 @@
       flake = false;
     };
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgsold.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, flake-utils, nixpkgs, djvu2pdf-git, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    , nixpkgsold
+    , djvu2pdf-git
+    , ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pkgsold = nixpkgsold.legacyPackages.${system};
 
-          ocrodjvu =
-            let
-              buildPythonPackage = pkgs.python3Packages.buildPythonPackage;
-              python-djvulibre = pkgs.callPackage ./pkgs/python-djvulibre.nix {
-                inherit buildPythonPackage;
-                cython = pkgs.python3Packages.cython;
-              };
-            in
-            pkgs.callPackage ./pkgs/ocrodjvu.nix {
-              inherit buildPythonPackage python-djvulibre;
-              lxml = pkgs.python3Packages.lxml;
-            };
+        ocrodjvu = pkgs.ocrodjvu;
 
-          python3 = pkgs.python3;
+        python3 = pkgs.python3;
 
-          pdfbeads = pkgs.callPackage ./pkgs/pdfbears.nix { ruby = pkgs.ruby_3_1; };
+        pdfbeads = pkgsold.callPackage ./pkgs/pdfbears.nix { ruby = pkgsold.ruby_3_1; };
 
-          djvu2pdf-toc-parser = pkgs.stdenv.mkDerivation (finalAttrs: {
-            pname = "djvu2pdf-toc-parser";
-            version = "";
+        djvu2pdf-toc-parser = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "djvu2pdf-toc-parser";
+          version = "";
 
-            src = inputs.djvu2pdf-git;
+          src = inputs.djvu2pdf-git;
 
-            nativeBuildInputs = [ ];
-            buildInputs = [ ];
-            buildPhase = "";
-            installPhase = ''
-              mkdir -p $out/bin;
-              cp djvu2pdf_toc_parser.py $out/bin/djvu2pdf_toc_parser.py
-            '';
-            meta = {
-              description = "generates compressed PDF from DjVu and tries to include text layers from the original DjVu file. I have no idea what happens in case there is no embedded text.";
-              homepage = "https://github.com/letac/djvu2pdf";
-              license = pkgs.lib.licenses.mit;
-            };
-          });
-          text = builtins.readFile "${djvu2pdf-git}/djvu2pdf";
-          djvu2pdf = pkgs.writeShellApplication {
-            name = "djvu2pdf";
-            runtimeInputs = [
-              djvu2pdf-toc-parser
-              python3
-              ocrodjvu
-
-              pkgs.djvulibre
-              pkgs.libtiff
-              pdfbeads
-            ];
-
-            text = text;
+          nativeBuildInputs = [ ];
+          buildInputs = [ ];
+          buildPhase = "";
+          installPhase = ''
+            mkdir -p $out/bin;
+            cp djvu2pdf_toc_parser.py $out/bin/djvu2pdf_toc_parser.py
+          '';
+          meta = {
+            description = "generates compressed PDF from DjVu and tries to include text layers from the original DjVu file. I have no idea what happens in case there is no embedded text.";
+            homepage = "https://github.com/letac/djvu2pdf";
+            license = pkgs.lib.licenses.mit;
           };
-        in
-        {
-          formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
-          packages = {
-            djvu2pdf = djvu2pdf;
-            default = self.packages.${system}.djvu2pdf;
-          };
+        });
+        text = builtins.readFile "${djvu2pdf-git}/djvu2pdf";
+        djvu2pdf = pkgs.writeShellApplication {
+          name = "djvu2pdf";
+          runtimeInputs = [
+            djvu2pdf-toc-parser
+            python3
+            ocrodjvu
 
-          devShell = pkgs.mkShell {
-            packages = [
-              self.packages.${system}.djvu2pdf
-            ];
-          };
-        }
-      );
+            pkgs.djvulibre
+            pkgs.libtiff
+            pdfbeads
+          ];
+
+          text = text;
+        };
+      in
+      {
+        formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
+        packages = {
+          djvu2pdf = djvu2pdf;
+          default = self.packages.${system}.djvu2pdf;
+        };
+
+        devShell = pkgs.mkShell {
+          packages = [
+            self.packages.${system}.djvu2pdf
+          ];
+        };
+      }
+    );
 }
-
